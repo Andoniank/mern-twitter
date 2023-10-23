9bc62ab99458a3d756b92ef6026d8335d0cf5c48
Andoniank <andoniank@gmail.com>
2023-10-22 18:07:20 -0700

    Finish User Auth

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,10 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+require('./models/User');
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); // <-- ADD THIS LINE
+
 const usersRouter = require('./routes/api/users'); // update the import file path
 const tweetsRouter = require('./routes/api/tweets');
 const csrfRouter = require('./routes/api/csrf');
@@ -17,6 +21,7 @@ app.use(logger('dev')); // log request components (URL/method) to terminal
 app.use(express.json()); // parse JSON request body
 app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
 app.use(cookieParser()); // parse cookies as an object on req.cookies
+app.use(passport.initialize());
 
 // Security Middleware
 if (!isProduction) {
@@ -38,7 +43,7 @@ app.use(
 );
 
 // Attach Express routers
-app.use('/api/users', usersRouter); // update the path
+app.use('/api/users', usersRouter); 
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
